d000940fd48aa92913ddb68c478d32f145500753
Catherine Simmons <simmecat@gmail.com>
2023-04-13 14:10:46 -0500

    Update bio and resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,10 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <Text>
-      Catherine Simmons is a Game Design student at DePaul University and a research assistant for <a className="hyperlink" href="https://sites.google.com/aegisgrowth.com/projectname/home?authuser=0" target="_blank">Aegis Growth</a>.
+        Catherine Simmons is a game design student at DePaul University who specializes in Production. She has worked as a grader for an Introduction to Game Production course and as a research assistant for ARG game research.
       </Text>
       <Text>
-        Formerly a Film major, Catherine designed one game in an elective class and was hooked for life. Catherine switched her major and has been working on projects as a producer, designer, and writer ever since.
-      </Text>
-      <Text>
-        Catherine is a student in Chicago, but based out of San Diego. In her free time, she enjoys playing video games, reading, writing, crochet, and finding any excuse to walk several miles around Chicago.
+        Catherine is passionate about healthy and playful game production and is always excited to help people execute their goals!
       </Text>
     </div>
   );
