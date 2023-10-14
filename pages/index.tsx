221e49a8bb418987c6b0798ebdc4b898f4b220c9
mallen54 <mjallen247@gmail.com>
2023-10-14 15:25:58 -0500

    Update resume and bio; new portfolio adds

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,21 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <Text>
-        Catherine Simmons is a game design student at DePaul University who specializes in Production. She has worked as a grader for an Introduction to Game Production course and as a research assistant for ARG game research.
+      Hi! I am a Game Design major at DePaul University with a passion for production and project
+      management. I am an organizer and communicator to my core. I love acting as a support on
+      teams to help everyone reach their full potential!
+      I transferred from the University of Kansas to DePaul University in the fall of 2020, took a game
+      design course, and promptly switched my major from film to game design. After taking a course
+      called Introduction to Game Production, I decided to concentrate in production and project
+      management.
       </Text>
       <Text>
-        Catherine is passionate about healthy and playful game production and is always excited to help people execute their goals!
+      I have worked as a research assistant/project manager, a teaching assistant, a game design
+      instructor, and now as a producer for DePaul Originals Game Studio.
+      Outside of DePaul, I am a hostess at a Brewery and I enjoy traveling, film photography,
+      watching basketball, and doing game jams with friends.
       </Text>
+      
     </div>
   );
 }
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -7,11 +7,23 @@ import brickBreaker from "../../public/images/brick-breaker.png";
 import whc from "../../public/images/whc.png";
 import gentlemenPirates from "../../public/images/gentlemen-pirates.png";
 import valoProductionHub from "../../public/images/valo-production-hub.png";
+import princessClock from "../../public/images/princessClock.png";
+import squirrelsWinter from "../../public/images/squirrelsWinter.png";
+import templeOfGeorge from "../../public/images/templeOfGeorge.png";
 
 export default function Portfolio() {
   return (
     <div className="flex flex-col gap-2">
       <div className="flex flex-row gap-2 justify-around">
+        <div className="relative h-[175px] w-[175px] md:h-[428px] md:w-[428px]">
+          <a href="https://pine-parrot-c38.notion.site/Production-Hub-VALORANT-0958d2b05eb4430387a6cc1b68f2faa5" target="_blank">
+            <Image
+              src={valoProductionHub}
+              alt="Valorant Production Hub"
+              className="rounded"
+            />
+          </a>
+        </div>
         <div className="relative h-[175px] w-[175px] md:h-[428px] md:w-[428px]">
           <Link href="/portfolio/gentlemen-pirates">
             <a>
@@ -22,15 +34,6 @@ export default function Portfolio() {
               />
             </a>
           </Link>
-        </div>
-        <div className="relative h-[175px] w-[175px] md:h-[428px] md:w-[428px]">
-          <a href="https://pine-parrot-c38.notion.site/Production-Hub-VALORANT-0958d2b05eb4430387a6cc1b68f2faa5" target="_blank">
-            <Image
-              src={valoProductionHub}
-              alt="Valorant Production Hub"
-              className="rounded"
-            />
-          </a>
         </div>
       </div>
       <div className="flex flex-row gap-2 justify-around">
@@ -71,6 +74,43 @@ export default function Portfolio() {
               <Image
                 src={modelingAnimation}
                 alt="3D Modeling & Animation"
+                className="rounded"
+              />
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-row gap-2 justify-around">
+        <div className="relative h-[175px] w-[175px] md:h-[428px] md:w-[428px]">
+          <Link href="https://catherinesimmons.itch.io/temple-of-george">
+            <a>
+              <Image
+                src={templeOfGeorge}
+                alt="Temple of George"
+                className="rounded"
+              />
+            </a>
+          </Link>
+        </div>
+        <div className="relative h-[175px] w-[175px] md:h-[428px] md:w-[428px]">
+          <Link href="https://catherinesimmons.itch.io/the-princess-and-the-clock">
+            <a>
+              <Image
+                src={princessClock}
+                alt="The Princess and the Clock"
+                className="rounded"
+              />
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-row gap-2 justify-around">
+        <div className="relative h-[175px] w-[175px] md:h-[428px] md:w-[428px]">
+          <Link href="https://azurecoffin.itch.io/squirrels-first-winter">
+            <a>
+              <Image
+                src={squirrelsWinter}
+                alt="Squirrels First Winter"
                 className="rounded"
               />
             </a>
